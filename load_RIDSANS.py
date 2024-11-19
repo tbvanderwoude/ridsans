d3d79f4ef912bf7ec70ccfa943129e1a97017f49
Thom van der Woude <tbvanderwoude@protonmail.com>
2024-11-19 11:39:56 +0100

    Added some documentation to load_RIDSANS and reformatted using black

--- load_RIDSANS.py
+++ load_RIDSANS.py
@@ -4,6 +4,7 @@ from mantid.kernel import *
 from sansdata import *
 from multiprocessing import Pool
 
+
 def load_measurement_files(
     sample_scatter_file,
     sample_transmission_file,
@@ -12,8 +13,18 @@ def load_measurement_files(
     background_file,
     plot_measurements=False,
 ):
+    """Loads all needed measurement files as SansData objects and plots these if plot_measurements is set. Uses a multiprocessing pool to speed up loading of files."""
     with Pool(5) as p:
-        sample_scatter, sample_transmission, can_scatter, direct, background = p.map(SansData, [sample_scatter_file, sample_transmission_file, can_scatter_file, direct_file, background_file])
+        sample_scatter, sample_transmission, can_scatter, direct, background = p.map(
+            SansData,
+            [
+                sample_scatter_file,
+                sample_transmission_file,
+                can_scatter_file,
+                direct_file,
+                background_file,
+            ],
+        )
     if plot_measurements:
         sample_scatter.plot_2d(True)
         sample_transmission.plot_2d(True)
@@ -23,6 +34,7 @@ def load_measurement_files(
 
 
 def create_pixel_adj_workspace(pixel_efficiencies, bins, detectors):
+    """Creates a workspace from a NumPy array of pixel efficiencies to be used in Qxy or Q1D as PixelAdj."""
     x = np.tile(bins, detectors)
     y = pixel_efficiencies
     y[y <= 0] = 1
@@ -37,12 +49,13 @@ def create_pixel_adj_workspace(pixel_efficiencies, bins, detectors):
 
 
 def compute_tranmission_factor(sample_transmission, direct):
-    """Compute tranmission factor assuming that the same attenuator is used, needs to be adjusted otherwise"""
+    """Compute tranmission factor assuming that the same attenuator is used, needs to be adjusted otherwise."""
     # TODO: Consider ROI for more accurate tranmission estimate (less noise)
     return np.sum(sample_transmission.I) / np.sum(direct.I)
 
 
 def monochromatic_workspace(name, I, detector_position, bins, detectors):
+    """Creates a monochromatic Mantid workspace from intensity I (can also be counts) together with precomputed bins and a detector position (relative to sample) along beam axis."""
     # Use the same bin for each detector
     x = np.tile(bins, detectors)
 
@@ -50,6 +63,9 @@ def monochromatic_workspace(name, I, detector_position, bins, detectors):
         OutputWorkspace=name, UnitX="Wavelength", DataX=x, DataY=I, NSpec=detectors
     )
     mon = LoadInstrument(ws, FileName="RIDSANS_Definition.xml", RewriteSpectraMap=True)
+
+    # Move sample to right position relative to detector
+    # This needs to be set correctly for later reduction steps (Qxy/Q1D)
     MoveInstrumentComponent(
         ws,
         ComponentName="sample-position",
@@ -77,9 +93,7 @@ def workspace_from_measurement(
     detectors,
 ):
     """Creates a workspace with scaled intensity in counts/s"""
-    # Use the same bin for each detector
-    x = np.tile(bins, detectors)
-    # Can (container) transmission factor
+    # Sample transmission factor
     T_sample = compute_tranmission_factor(sample_transmission, direct)
     # Can (container) transmission factor
     T_can = (
@@ -98,6 +112,7 @@ def workspace_from_measurement(
 
 
 def create_monochrom_bin_bounds(L0, delta_L_over_L0=0.1):
+    """Creates the wavelength bin boundaries from a central wavelength L0 with a certain spread."""
     bin_lower = L0 * (1.0 - delta_L_over_L0 / 2)
     bin_upper = L0 * (1.0 + delta_L_over_L0 / 2)
     return [bin_lower, bin_upper]
