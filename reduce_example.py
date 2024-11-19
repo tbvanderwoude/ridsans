06cbf9192fd4f5e2df1bfe0a2174a2b51a37f9be
Thom van der Woude <tbvanderwoude@protonmail.com>
2024-11-19 11:18:54 +0100

    Parametrized reduce_example by i similar to load_example

--- reduce_example.py
+++ reduce_example.py
@@ -1,10 +1,12 @@
 from reduce_RIDSANS import *
 from mantid.api import AnalysisDataService as ADS
 
+i = 2
 active_w = 0.15  # m
 active_h = 0.15  # m
-ws_sample = ADS.retrieve("data/sample1_Q4.mpa")
-ws_direct = ADS.retrieve("data/no_cuvette_transmission_Q4.mpa")
+
+ws_sample = ADS.retrieve(f"data/sample1_Q{i}.mpa")
+ws_direct = ADS.retrieve(f"data/no_cuvette_transmission_Q{i}.mpa")
 ws_pixel_adj = ADS.retrieve("PixelAdj")
 
 reduction_setup_RIDSANS(ws_sample, ws_direct, active_w, active_h)
