6a48df2301ea8eeb63573276aaa6be0fce4c17a5
Thom van der Woude <tbvanderwoude@protonmail.com>
2024-12-18 18:16:37 +0100

    Updated various examples to be compatible with sample-data

--- batch_load_example.py
+++ batch_load_example.py
@@ -1,6 +1,6 @@
 from batch_processing_RIDSANS import *
 
-for index in range(0, 4):
+for index in range(4,8):
     ws_sample, ws_direct, mon, ws_pixel_adj = load_batchfile_index_workspaces(
-        index, "pixel-efficiency.txt.gz", "sans-batchfile.csv"
+        index, "pixel-efficiency.txt.gz", "sans-batchfile.csv", directory = "sample-data"
     )
--- batch_reduce_example.py
+++ batch_reduce_example.py
@@ -3,9 +3,9 @@ from reduce_RIDSANS import *
 from mantid.api import AnalysisDataService as ADS
 
 # mask_workspace = ADS.retrieve('MaskWorkspace')
-for index in range(0, 4):
+for index in range(4,8):
     ws_sample, ws_direct, mon, ws_pixel_adj = load_batchfile_index_workspaces(
-        index, "pixel-efficiency.txt.gz", "sans-batchfile.csv"
+        index, "pixel-efficiency.txt.gz", "sans-batchfile.csv", directory = "sample-data"
     )
     print(index, ws_sample.name())
     
