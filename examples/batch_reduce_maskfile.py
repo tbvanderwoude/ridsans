f3fd537f94b07aadccd98edf7e9ce8f2d2197fed
Thom van der Woude <tbvanderwoude@gmail.com>
2025-02-27 20:13:03 +0100

    Added batch reduce example using maskfile

---
+++ examples/batch_reduce_maskfile.py
@@ -0,0 +1,15 @@
+from ridsans.batch_processing import *
+from ridsans.reduce import *
+from mantid.api import AnalysisDataService as ADS
+
+for index in range(0, 4):
+    i = index + 1
+    # Requires mask files to be previously created and saved, possibly
+    # using the View Instrument GUI
+    mask = LoadMask("RIDSANS_Definition.xml", f"Q{i}_mask.xml")
+    ws_sample, ws_direct, mon, ws_pixel_adj = load_batchfile_index_workspaces(
+        index, "pixel-efficiency.txt.gz", "sans-batchfile.csv", directory="sample-data"
+    )
+    print(index, ws_sample.name())
+    reduction_setup_RIDSANS(ws_sample, ws_direct, mask_workspace=mask)
+    reduced_ws_1D = reduce_RIDSANS_1D(ws_sample, ws_pixel_adj)
