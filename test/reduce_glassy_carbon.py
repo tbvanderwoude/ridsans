2f7d313eacbfaa2dcc914a1066ed673976978aad
Thom van der Woude <tbvanderwoude@gmail.com>
2025-03-04 17:22:22 +0100

    Changed glassy carbon reduction to use mask files

--- test/reduce_glassy_carbon.py
+++ test/reduce_glassy_carbon.py
@@ -2,12 +2,17 @@ from ridsans.batch_processing import *
 from ridsans.reduce import *
 from mantid.api import AnalysisDataService as ADS
 
-for index in range(0,4):
+for index in range(0, 4):
+    i = index + 1
+    mask = LoadMask("RIDSANS_Definition.xml", f"Q{i}_mask.xml")
     ws_sample, ws_direct, mon, ws_pixel_adj = load_batchfile_index_workspaces(
-        index, "pixel-efficiency.txt.gz", "test.csv", directory="test-data",
+        index,
+        "pixel-efficiency.txt.gz",
+        "test.csv",
+        directory="test-data",
     )
     print(index, ws_sample.name())
 
-    reduction_setup_RIDSANS(ws_sample, ws_direct, mask_workspace=None)
+    reduction_setup_RIDSANS(ws_sample, ws_direct, mask_workspace=mask)
 
     reduced_ws_1D = reduce_RIDSANS_1D(ws_sample, ws_pixel_adj)
