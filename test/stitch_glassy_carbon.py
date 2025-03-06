e1f00879d6e7ff842c742628878b6c49538c0c2f
Thom van der Woude <tbvanderwoude@protonmail.com>
2025-03-06 17:09:36 +0100

    Added printing of scale factors to stitch_glassy_carbon

--- test/stitch_glassy_carbon.py
+++ test/stitch_glassy_carbon.py
@@ -7,4 +7,5 @@ from ridsans.save import save
 
 workspaces = [ADS.retrieve(f"scattering_0_25mm_glassy_C_Q{i}_dSigma/dOmega_1D") for i in range(1, 5)]
 stitched_ws, scale_factors = stitch_Q_ranges(workspaces)
+print(scale_factors)
 save(stitched_ws, 'glassy_carbon')
