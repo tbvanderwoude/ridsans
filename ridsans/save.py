98beada850166d4b84c6f5d18173cf16f33ace99
Thom van der Woude <tbvanderwoude@gmail.com>
2025-05-16 17:19:38 +0200

    Added prototype save_2D function for saving workspaces to 2D CanSAS format

--- ridsans/save.py
+++ ridsans/save.py
@@ -2,7 +2,6 @@ from mantid.api import *
 from mantid.kernel import *
 from mantid.simpleapi import *
 
-
 def save(workspace, file_name=None):
     """Wrapper around SaveCanSAS1D that appends the .xml extension to the filename if needed."""
     if file_name is None:
@@ -10,3 +9,24 @@ def save(workspace, file_name=None):
     elif not file_name.endswith(".xml"):
         file_name += ".xml"
     return SaveCanSAS1D(workspace, file_name)
+
+def save_2D(workspace, file_name=None):
+    """Wrapper around SaveNXcanSAS that appends the .h5 extension to the filename if needed."""
+    if file_name is None:
+        file_name = f"{workspace.name()}.h5"
+    elif not file_name.endswith(".h5"):
+        file_name += ".h5"
+    
+    # TODO: add proper file_name sanitization
+    file_name = file_name.replace("/", "_").replace("\\", "_")
+
+    # Convert the workspace to units of momentum transfer (in case it is not already)
+    workspace = ConvertUnits(workspace,Target="MomentumTransfer")
+
+    # Not the actual instrument, might break stuff. RIDSANS is not officially known anywhere yet
+    # so this needs to be set as otherwise SaveNXcanSAS complains no institute uses a RIDSANS 
+    LoadInstrument(workspace,False,InstrumentName="SANS2D")
+
+    # Save the file
+    SaveNXcanSAS(workspace,file_name)
+
